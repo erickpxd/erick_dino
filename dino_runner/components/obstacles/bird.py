58210fa3f2b98775e40f8a9cf36db8a66abdb203
erickpxd <erick.monteiroeflm@gmail.com>
2023-08-15 18:00:48 -0300

    RESTART AND BIRD OK

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -16,7 +16,7 @@ class Bird(Obstacle):
             self.rect.y = 225
     
     def draw(self, screen):
-        if self.step_index >= 10:
-            self.step_index = 0
-        screen.blit(self.image[self.step_index < 5], self.rect)
+        screen.blit(self.image[self.step_index // 5], self.rect)
         self.step_index += 1
+        if self.step_index >= 9:
+            self.step_index = 0
